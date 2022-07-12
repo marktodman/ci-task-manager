e7ea031c49b3ad85e59e146c53c1c2cb0066f3cd
marktodman <drmtodman@gmail.com>
2022-07-12 13:19:38 +0000

    feat: Add defensive code to check Category deletion via modal confirmation

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // sidebar initialization
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+document.addEventListener("DOMContentLoaded", function() {
+  // sidenav initialization
+  let sidenav = document.querySelectorAll(".sidenav");
+  M.Sidenav.init(sidenav);
+
+  // category deletion modal
+  let modal = document.querySelectorAll('.modal');
+  M.Modal.init(modal);
+
+  // datepicker initialization
+  let datepicker = document.querySelectorAll(".datepicker");
+  M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
   });
+
+  // select initialization
+  let selects = document.querySelectorAll("select");
+  M.FormSelect.init(selects);
+});
